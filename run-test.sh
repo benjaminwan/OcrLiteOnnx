886f20ebb9ca29ff95854191c3cd8a4b640742ca
benjaminwan <benjaminwan@163.com>
2020-10-21 10:21:38 +0800

    test脚本添加OMP_NUM_THREADS参数

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+#### Setting the Number of Threads Using an OpenMP* Environment Variable
+set OMP_NUM_THREADS=4
+
 ##### run test on MacOS or Linux
 pushd build
-./OcrLiteOnnx ../test_imgs/1.jpg ../models 0 50 0 0.6 0.3 3 1.3 1.3 1.6 1.6
+./OcrLiteOnnx ../test_imgs/1.jpg ../models 4 50 0 0.6 0.3 3 1.3 1.3 1.6 1.6
 popd
