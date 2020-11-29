9cadd75eb664cfb636d6fed65de9b2643614450c
benjaminwan <benjaminwan@163.com>
2020-11-29 17:23:22 +0800

    修改generate-vs-project.cmd

--- generate-vs-project.cmd
+++ generate-vs-project.cmd
@@ -58,7 +58,7 @@ popd
 GOTO:EOF
 
 :cmakeParams
-cmake -G "%~1" -A "%~2" -DCMAKE_BUILD_TYPE=Release -DOCR_LITE_OPENMP=OFF ..
+cmake -G "%~1" -A "%~2" -DCMAKE_BUILD_TYPE=Release -DOCR_LITE_OPENMP=ON ..
 GOTO:EOF
 
 @ENDLOCAL
