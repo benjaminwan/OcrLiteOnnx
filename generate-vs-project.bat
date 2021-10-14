e95d3b4f5bf4f99702dcc68b2c99aa2e004e9f52
benjaminwan <benjaminwan@163.com>
2021-10-14 17:51:54 +0800

    fix: generate-vs-project.bat

--- generate-vs-project.bat
+++ generate-vs-project.bat
@@ -25,35 +25,17 @@ else if %flag% == 3 (set BUILD_OUTPUT="CLIB")^
 else (echo 输入错误！Input Error!)
 echo.
 
-echo "请输入选项并回车: 0)ALL, 1)vs2017-x86, 2)vs2017-x64, 3)vs2019-x86, 4)vs2019-x64:"
+echo "请输入选项并回车: 0)ALL, 1)vs2019-x86, 2)vs2019-x64:"
 set /p flag=
 if %flag% == 0 (call :buildALL)^
-else if %flag% == 1 (call :gen2017-x86)^
-else if %flag% == 2 (call :gen2017-x64)^
-else if %flag% == 3 (call :gen2019-x86)^
-else if %flag% == 4 (call :gen2019-x64)^
+else if %flag% == 1 (call :gen2019-x86)^
+else if %flag% == 2 (call :gen2019-x64)^
 else (echo "输入错误！Input Error!")
 GOTO:EOF
 
 :buildALL
-call :gen2017-x86
-call :gen2017-x64
 call :gen2019-x86
 call :gen2019-x64
-GOTO:EOF
-
-:gen2017-x86
-mkdir build-win-vs2017-x86
-pushd build-win-vs2017-x86
-call :cmakeParams "Visual Studio 15 2017" "Win32"
-popd
-GOTO:EOF
-
-:gen2017-x64
-mkdir build-win-vs2017-x64
-pushd build-win-vs2017-x64
-call :cmakeParams "Visual Studio 15 2017" "x64"
-popd
 GOTO:EOF
 
 :gen2019-x86
